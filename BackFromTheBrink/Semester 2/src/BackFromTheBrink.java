716b60c9a7fa350ccba93701155e017881397e48
Jaspier <57241026+jaspier@users.noreply.github.com>
2021-02-03 20:45:28 +0000

    Finished set up board method

--- BackFromTheBrink/Semester 2/src/BackFromTheBrink.java
+++ BackFromTheBrink/Semester 2/src/BackFromTheBrink.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 public class BackFromTheBrink {
 
+    public static class Biome {
+        public Biome(String name, ArrayList habitats) {
+            name = name;
+            habitats = habitats;
+        }
+    }
+
+
     public static class SpottedByPredator extends Square{
 
         public SpottedByPredator(String newName, int nextPos) {
@@ -22,8 +30,6 @@ public class BackFromTheBrink {
         Square spottedInSafari = new SpottedByPredator("SpottedByPredator", 30);
         board.addSquare(spottedInSafari);
         //board.setupBoard();
-
-        PlayerRegistration.BeginRegistration();
 
         while(getPlayersInGame(players) > 1 || !getBftbWon()) {
             playerTurnHandler();
@@ -104,8 +110,8 @@ public class BackFromTheBrink {
     public static void turnOptions(int option) {
         switch(option) {
             case 1: //end turn
-            case 2: //build zoo()
-            case 3: //trade()
+            case 1: //build zoo()
+            case 1: //trade()
         }
     }
 }
