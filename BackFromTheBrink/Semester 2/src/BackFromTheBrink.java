e8e97f522610288690df2ccdfd8617511d7d104d
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2021-02-06 12:59:33 -0600

    completed trade method just needs to be checked and tested, couple changes to the Inventory and StdIO class

--- BackFromTheBrink/Semester 2/src/BackFromTheBrink.java
+++ BackFromTheBrink/Semester 2/src/BackFromTheBrink.java
@@ -5,6 +5,7 @@ public class BackFromTheBrink {
     static ArrayList<Player> players = new ArrayList<Player>();
     static ArrayList<Player> outOfGame = new ArrayList<Player>();
     private static boolean bftbWon = false;
+    private static StdIO IO;
     static Board board;
 
     static Dice di = new Dice();
@@ -43,12 +44,12 @@ public class BackFromTheBrink {
     public static void playerTurnHandler() {
         //print statements
         for (int i = 0; i < players.size(); i++) {
-            Scanner sc = new Scanner(System.in);
+            Player currentPlayer = players.get(i);
             //check if player is in jail/safari
-            if(players.get(i).isInSafari()){
+            if(currentPlayer.isInSafari()){
                 //print out options
-                int option = sc.nextInt();
-                inSafariOptions(option, players.get(i));
+                int option = IO.readInt();
+                inSafariOptions(option, currentPlayer);
             }
             else {
 
@@ -57,60 +58,103 @@ public class BackFromTheBrink {
 
                 //move around board
                 //moveAroundBoard(rollValue);
-                players.get(i).getPiece().move(rollValue);
+                currentPlayer.getPiece().move(rollValue);
 
 
                 if (di.getIsDouble()) {
                     int rollValue2 = di.getRollValue();
                     // move around board
                     //moveAroundBoard(rollValue);
-                    players.get(i).getPiece().move(rollValue);
+                    currentPlayer.getPiece().move(rollValue);
                 }
 
                 if (di.getIsSecondDouble()) {
                     // go to jail
-                    players.get(i).setInSafari(true);
+                    currentPlayer.setInSafari(true);
                     // move player to the spotted in the safari sqaure
-                    players.get(i).getPiece().move(board.getSquare(30));
+                    currentPlayer.getPiece().move(board.getSquare(30));
                 }
 
                 //print out all options available
                 //request input
-                int option = sc.nextInt();
+                int option = IO.readInt();
                 while (option != 9) {
-                    turnOptions(option);
+                    turnOptions(option, currentPlayer);
                 }
             }
         }
     }
 
-    public static void inSafariOptions(int option, Player player) {
+    public static void inSafariOptions(int option, Player currentPlayer) {
         switch(option) {
-            case 1: di.tryDouble(player);
-            case 2: player.deductMaterials(50);
-            case 3: player.useWildCard(player);
+            case 1: di.tryDouble(currentPlayer);
+            case 2: currentPlayer.deductMaterials(50);
+            case 3: currentPlayer.useWildCard(currentPlayer);
         }
     }
 
-    public static void turnOptions(int option) {
+    public static void turnOptions(int option, Player currentPlayer) {
         switch(option) {
-            case 1: //end turn
-            case 2: //build zoo()
-            case 3: //trade()
+            case 1: //endTurn;
+            case 2: //buildZoo();
+            case 3: trade(currentPlayer);
         }
     }
 
-    public static void trade() {
+    public static void trade(Player currentPlayer) {
         //print or display all players
-        for(Player player: players) {
-            System.out.println(player.getInventory().getBiomesInfo());
-        }
+        IO.printAllPlayers(players);
         //request user input to select player they want to trade with
-
+        int input = IO.readInt()-1;
         //print selected players habitats
+        if(input >= players.size() || input < 0) {
+            System.out.println("This player does not exist!");
+            return;
+        }
+        Player selectedPlayer = players.get(input);
+        IO.printPlayer(selectedPlayer);
+        //trade verification
+        System.out.println("Are you sure you want to trade with this player? Type y for yes, n for no");
         //request user input
-        //request user how much they want to pay for habitat
-        //request user2 if they want to accept
+        String verification = IO.read().toLowerCase();
+        if (verification.equals("y")){
+            //ask user which biome they want to buy from
+            System.out.println("Select the biome you would like to buy from");
+            for(int i = 0; i < selectedPlayer.getInventory().getBiomes().size(); i++) {
+                IO.printBiomes(selectedPlayer);
+            }
+            //request user input
+            input = IO.readInt()-1;
+            if(input >= selectedPlayer.getInventory().getBiomes().size() || input < 0) {
+                System.out.println("This biome does not exist!");
+                return;
+            }
+            Biome selectedBiome = selectedPlayer.getInventory().getBiomes().get(input);
+            //request which habitat they want to buy from
+            System.out.println("Which Habitat would you like to buy?");
+            input = IO.readInt()-1;
+            Habitat selectedHabitat = selectedBiome.getAnimalHabitats().get(input);
+            //request user how much they want to pay for habitat
+            System.out.println("How much would you like to pay for it?");
+            int amount = IO.readInt();
+            //request user2 if they want to accept
+            System.out.println("Pass the remote controller to " + selectedPlayer.getName());
+            String tradeRequest = "";
+            tradeRequest += currentPlayer.getName() + " has requested to buy " + selectedHabitat.getName() +
+                    " in the " + selectedBiome.getName() + " biome for " + amount;
+            System.out.println("Would you like to accept this trade? y for yes, n for no ");
+            verification = IO.read();
+            //transaction body
+            if (verification.equals("y")) {
+                selectedPlayer.increasePlayerMaterials(amount);
+                currentPlayer.deductMaterials(amount);
+                selectedBiome.removeAnimalHabitat(selectedHabitat);
+                currentPlayer.getInventory().addHabitat(selectedBiome,selectedHabitat);
+            }
+        } else {
+            System.out.println("Trade Cancelled");
+            return;
+        }
     }
 
 }
--- BackFromTheBrink/Semester 2/src/StdIO.java
+++ BackFromTheBrink/Semester 2/src/StdIO.java
@@ -1,19 +1,22 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class StdIO {
-     public String printVar(Player player) {
-         //name
-         //inventory
-            //mats
-            //biomes
-            //habitats
-            //wildcards
-         //piece
-         //insafari status
-         String name = "Name: " + player.getName();
+    public String printAllPlayers(ArrayList<Player> playerList) {
+        String completeList = "";
+        int playerIndex = 1;
+        for(int i = 0; i < playerList.size(); i++) {
+            completeList += playerIndex + ". " + printPlayer(playerList.get(i));
+            playerIndex++;
+        }
+        return completeList;
+    }
+
+     public String printPlayer(Player player) {
+         String name = player.getName();
          String inventory = "Inventory: ";
          String materials = "Materials: " + player.getInventory().getMaterials();
-         String biomes = "Biomes and Habitats: " + player.getInventory().getBiomes().;
+         String biomes = "Biomes and Habitats: ";
          for (int i = 0; i < player.getInventory().getBiomes().size(); i++) {
              biomes +=  player.getInventory().getBiomes().get(i).getBiomeName();
                 biomes+= "\n";
@@ -31,9 +34,44 @@ public class StdIO {
          String status = "Status: " + "\n" + player.isInSafari();
          return name + "\n" + inventory + "\n" + materials + "\n" + biomes + "\n" + wildcards + "\n" + piece + "\n" + status;
      }
-    public String printVar(Inventory inventory) {
-        return "";
+
+    public String printInventory(Player player) {
+         String name = "Player: " + player.getName();
+        String biomes = "Biomes and Habitats: ";
+        for (int i = 0; i < player.getInventory().getBiomes().size(); i++) {
+            biomes +=  player.getInventory().getBiomes().get(i).getBiomeName();
+            biomes+= "\n";
+            for (int j = 0; j < player.getInventory().getBiomes().get(i).getAnimalHabitats().size(); j++) {
+                biomes += "\n";
+                biomes += player.getInventory().getBiomes().get(i).getAnimalHabitats().get(j).getName();
+            }
+        }
+        String wildcards = "Wildcards: ";
+        for(WildCard wildCard: player.getInventory().getWildCard()) {
+            wildcards += "\n";
+            wildcards += wildCard.getName();
+        }
+        return biomes + "\n" + wildcards;
     }
+
+    public String printBiomes(Player player) {
+        String name = "Player: " + player.getName();
+        String biomes = "Biomes and Habitats: ";
+        int biomeIndex = 1;
+        for (int i = 0; i < player.getInventory().getBiomes().size(); i++) {
+            biomes += biomeIndex + ". " + player.getInventory().getBiomes().get(i).getBiomeName();
+            biomes+= "\n";
+            biomeIndex++;
+            int habitatIndex = 1;
+            for (int j = 0; j < player.getInventory().getBiomes().get(i).getAnimalHabitats().size(); j++) {
+                biomes += "\n";
+                biomes += habitatIndex + ". " + player.getInventory().getBiomes().get(i).getAnimalHabitats().get(j).getName();
+                habitatIndex++;
+            }
+        }
+        return biomes;
+    }
+
     public String read() {
         System.out.println("\nPlayer Input:");
         System.out.println(">");
@@ -44,5 +82,24 @@ public class StdIO {
         }
         catch (IOException ex) { ex.printStackTrace(); }
         return value;
+    }
+
+    public int readInt() {
+        System.out.println("\nPlayer Input:");
+        System.out.println(">");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String value = null;
+        int intValue = 0;
+        try{
+            value = input.readLine();
+            value = value.replaceAll("\\s", "");
+            try {
+                intValue= Integer.parseInt(value);
+            }catch (NumberFormatException e) {
+                System.out.println("Invalid Input! Not an Integer!");
+            }
+        }
+        catch (IOException ex) { ex.printStackTrace(); }
+        return intValue;
     }
 }
