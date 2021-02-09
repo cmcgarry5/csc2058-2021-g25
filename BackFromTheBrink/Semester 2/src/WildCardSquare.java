836b3012783cfc5e284e93daac2c4eca4d963b9f
ChrisChanKho <46541246+ChrisChanKho@users.noreply.github.com>
2021-02-09 20:49:08 +0000

    Update WildCardSquare.java

--- BackFromTheBrink/Semester 2/src/WildCardSquare.java
+++ BackFromTheBrink/Semester 2/src/WildCardSquare.java
@@ -4,36 +4,10 @@ public class WildCardSquare extends Square{
 		super(name, position);
 	}
 
-	public void execute(Player player, int function) {
-		switch(function) {
-			case 1: //Advance to Jaguar
-				//player.getPiece().move();
-				break;
-			case 2: //Advance to Blue Whale
-				//player.getPiece().move();
-				break;
-			case 3: //Advance to nearest river
-				//player.getPiece().move();
-				break;
-			case 4: //Advance to Travel Square
-				//player.getPiece().move();
-				break;
-			case 5: //Go back 3 square
-				player.getPiece().move(-3);
-				break;
-			case 6: //Hide in safari
-				//player.getPiece().move()
-			case 7: //receive escape from safari card
-				//player.getInventory().addWildCard();
-			case 8: //Conservation grant +200 materials
-				//player.get
-			case 9: //grounds maintenance, 40 per zoo, 75 per park
-			case 10: //breeding program, 100 for each owned habitat
-			case 11: //elected chairman pay 50
-			case 12: //end of the month, pay 100
-			case 13: //conservation fee refund collect 200
-			case 14: //collect 50 off every player
-			case 15: //use escape from safari card
-		}
+	public void execute(Player player) {
+		WildCard wildCardDrawn = BackFromTheBrink.board.draw();
+		wildCardDrawn.execute(player);
 	}
+
 }
+
