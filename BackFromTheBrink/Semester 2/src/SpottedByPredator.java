40ccf1c99fec0ee3df1a2f4141230cbdc5004d6c
40263468 <cmcgarry18@qub.ac.uk>
2021-02-09 17:22:52 +0000

    Update SpottedByPredator.java

--- BackFromTheBrink/Semester 2/src/SpottedByPredator.java
+++ BackFromTheBrink/Semester 2/src/SpottedByPredator.java
@@ -1,7 +1,9 @@
 public class SpottedByPredator extends Square{
-
+Square hiding = new Square("HIDING", 10);
     public SpottedByPredator(String newName, int nextPos) {
         super(newName, nextPos);
+        System.out.println("Oh no! a predator has spotted you!\nGo into hiding!");
+    	player.getPiece().move(hiding);
     }
 }
 
