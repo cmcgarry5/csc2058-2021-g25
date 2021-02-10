64599d08dd26258e87039cd8fa5ea39736b4b480
ChrisChanKho <46541246+ChrisChanKho@users.noreply.github.com>
2021-02-10 00:43:55 +0000

    Update EscapeCard.java

--- BackFromTheBrink/Semester 2/src/EscapeCard.java
+++ BackFromTheBrink/Semester 2/src/EscapeCard.java
@@ -5,6 +5,10 @@ public class EscapeCard extends WildCard{
     }
 
     public void execute(Player player) {
-
+        if(player.getInventory().getWildCard().size() > 0){
+            Dice dice = new Dice();
+            int moveVal = dice.getRollValue();
+            player.getPiece().move(moveVal);
+        }
     }
 }
