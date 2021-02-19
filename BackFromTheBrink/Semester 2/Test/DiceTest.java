318cb59348e05718ce8f38ffa60eb9eb6d3f0490
Jaspier <57241026+jaspier@users.noreply.github.com>
2021-02-19 17:46:59 +0000

    TestCases Template

---
+++ BackFromTheBrink/Semester 2/Test/DiceTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceTest {
+
+    @Test
+    public void nextPlayer() {
+    }
+
+    @Test
+    public void getRollValue() {
+    }
+
+    @Test
+    public void getIsDouble() {
+    }
+
+    @Test
+    public void getIsSecondDouble() {
+    }
+
+    @Test
+    public void tryDouble() {
+    }
+}
